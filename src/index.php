01d908409ab2ab3162a8a0591a1749652cd91667
Hisham Dalal <hishamdalal@gmail.com>
2024-11-11 13:10:08 +0300

    fix index.php

--- src/index.php
+++ src/index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Browserify + Gulp Automation</title>
-    <link rel="stylesheet" href="bundle.css">
+    <link rel="stylesheet" href="bundle.css" />
 </head>
+
 <body>
     <header id="main-header">
         <nav>
