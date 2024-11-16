69f424b41368e643443c31bb9a9f8637b8fbf6ed
Hisham Dalal <hishamdalal@gmail.com>
2024-11-16 09:12:16 +0300

    Support minify images,  add mixins, function and code improvement and some fixes.

---
+++ gulpfile.mjs
@@ -0,0 +1,114 @@
+'use strict';
+
+import { src, dest, watch, series } from 'gulp';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+
+// const sass = require('gulp-sass')(require('sass'));
+// import dartSass from 'sass';
+// import gulpSass from 'gulp-sass';
+// const sass = gulpSass(dartSass);
+// import * as sass from 'sass';
+
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass);
+
+import postcss from 'gulp-postcss';
+import cssnano from 'cssnano';
+import terser from 'gulp-terser';
+import browser_sync from 'browser-sync';
+const browserSync = browser_sync.create();
+import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+
+// npm install -D gulp browserify babelify vinyl-source-stream vinyl-buffer gulp-sass sass gulp-postcss cssnano gulp-terser browser-sync gulp-sourcemaps gulp-imagemin 
+
+function php() {
+    return src('src/**/*.php')
+        .pipe(dest('dist'))
+}
+
+function scss() {
+    return src('src/scss/bundle.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(postcss([cssnano()]))
+        .pipe(sourcemaps.write())
+        .pipe(dest('dist/assets', { sourceMaps: '.' }))
+        .pipe(browserSync.stream());
+}
+
+function js() {
+    return browserify('./src/js/bundle.js', { debug: true, sourceMaps: true })
+        .transform(babelify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(terser())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(dest('dist/assets', { sourceMaps: '.' }))
+}
+
+function img() {
+        return src('src/img/*', { encoding: false })
+            .pipe(imagemin({
+                verbose: true
+              }))
+            .pipe(dest('dist/assets/img'))
+            .pipe(browserSync.stream());
+}
+
+// BrowserSync Tasks
+function browserSyncServe(cb) {
+    browserSync.init({
+        proxy: "http://localhost/workflow/Gulp/gulp-php-workflow/dist/",
+        port: 3000,
+        // server: {
+        //    baseDir: ['dist'],
+        // },
+    });
+    cb();
+}
+function browserSyncReload(cb) {
+    browserSync.reload();
+    cb();
+}
+function browserSyncStream(cb) {
+    browserSync.stream();
+    cb();
+}
+
+// Watch Task
+function watchTask() {
+    watch('src/*.php', series(php, browserSyncReload));
+    // watch('src/scss/**/*.scss', series(scss, browserSyncStream))
+    watch('src/scss/**/*.scss', series(scss, browserSyncReload))
+    watch('src/js/**/*.js', series(js, browserSyncReload))
+    watch('src/img/**', series(img, browserSyncReload))
+}
+
+export default series(
+    php,
+    scss,
+    js,
+    img,
+    browserSyncServe,
+    watchTask
+);
+
+export const build = series(
+    php,
+    scss,
+    js,
+    img
+);
+
+export const style = scss;
+export const script = js;
+export const file = php;
+export const image = img;
--- src/index.php
+++ src/index.php
@@ -1,10 +1,18 @@
+<?php
+require_once('functions.php');
+
+$url = full_url( $_SERVER );
+
+// $path = parse_url($url, PHP_URL_HOST);
+// echo __DIR__;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Browserify + Gulp Automation</title>
-    <link rel="stylesheet" href="bundle.css" />
+    <link rel="stylesheet" href="<?=$url?>assets/bundle.css" />
 </head>
 
 <body>
@@ -23,12 +31,25 @@
         <div class="container">
             <h1 class="hero">Hello world</h1>
             <button id="button">Counter 0</button>
+            <article class="post">
+                <img class="thumb" src="<?=$url?>assets/img/peter-livesey-unsplash.jpg" alt="">
+                <div class="text">
+                    <h3>Post title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias dolores, quasi aut, non pariatur esse, impedit voluptatem praesentium beatae architecto possimus? Quae delectus quisquam perferendis eius dolorem. Molestias, inventore consequuntur?</p>
+                    <a class="btn read-more" href="#">Read more</a>
+                </div>
+            </article>
         </div>
         <aside>
-
+            <h3 class="title">Title</h3>
+            <ul class="list">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+            </ul>
         </aside>
     </main>
         
-    <script src="bundle.js"></script>
+    <script src="assets/bundle.js"></script>
 </body>
 </html> 
