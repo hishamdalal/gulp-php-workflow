a5f7b234f941c94571687149a7b97117d605aac1
Hisham Dalal <hishamdalal@gmail.com>
2024-11-18 13:16:11 +0300

    support breakpoints

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@ $url = full_url( $_SERVER );
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Browserify + Gulp Automation</title>
+    <title>Gulp PHP Workflow</title>
     <link rel="stylesheet" href="<?=$url?>assets/bundle.css" />
 </head>
 
@@ -31,6 +31,8 @@ $url = full_url( $_SERVER );
         <div class="container">
             <div class="hero">
                 <h1>Hello world</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quas voluptates reprehenderit necessitatibus, sapiente impedit reiciendis, libero nulla? </p>
+                <p>Hic, provident ea cumque fuga adipisci voluptates non consequuntur dolores nobis eum facilis dolore asperiores tenetur quia laudantium est autem.</p>
                 <button id="button">Counter 0</button>
             </div>
             <article class="post">
@@ -51,7 +53,18 @@ $url = full_url( $_SERVER );
             </ul>
         </aside>
     </main>
-        
+
+    <div class="screen-size">
+        <div class="xxl">XXL</div>
+        <div class="xl">XL</div>
+        <div class="lg">LG</div>
+        <div class="md">MD</div>
+        <div class="sm">SM</div>
+        <div class="xs">XS</div>
+        <div class="xxs">XXS</div>
+        <div class="xxxs">XXXS</div>
+    </div>
+    
     <script src="<?=$url?>assets/bundle.js"></script>
 </body>
 </html> 
