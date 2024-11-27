bc3ce8d491bdb9d60564e34cce8c035035712ec7
Hisham Dalal <hishamdalal@gmail.com>
2024-11-27 13:26:29 +0300

    improve light dark theme and add switch button

--- src/index.php
+++ src/index.php
@@ -24,6 +24,22 @@ $url = full_url( $_SERVER );
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
+            <ul class="tools">
+                <li><form class="search-form"><input class="search-input" name="q" /><i class="search-icon"></i></form></li>
+                <li>
+                    <div class="light-dark-switch">
+                        <label><input type="radio" name="color-scheme" id="color-scheme-auto" checked>Auto</label>
+                        <label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                            <input type="radio" name="color-scheme" id="color-scheme-light">
+                        </label>
+                        <label>
+                            <input type="radio" name="color-scheme" id="color-scheme-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
+                        </label>
+                    </div>                    
+                </li>
+            </ul>
         </nav>
     </header>
 
--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,3 +1,5 @@
 const  {count} = require('./inc/counter');
+//const {dark_mode} = require('./inc/_dark_mode');
 
 count()
+//dark_mode();
